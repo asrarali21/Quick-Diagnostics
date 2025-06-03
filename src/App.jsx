40b4ce04987b8269fddd877355a9dbaa805681c5
Asrar Ali <mohdasrar3825@gmail.com>
2025-06-03 13:23:13 +0530

    implemented react recoil for global state

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,28 @@ import { useEffect, useState } from 'react';
 import './App.css'
 import Navbar from './components/Navbar';
 import Search from './components/Search';
+import { useSetRecoilState } from 'recoil';
+import { apiDataState } from './apiState';
 
 function App() {
+
+  const setApiData = useSetRecoilState(apiDataState)
+
+      useEffect(() => {
+       const FetchedData = async () => { 
+        try {
+              let response = await fetch("http://localhost:3001/tests")
+          let data = await response.json()
+          console.log(data)
+          setApiData(data)
+          
+        } catch (error) {
+          console.error("Failed to fetch data" , error)
+        }
+      
+       }
+       FetchedData()
+      }, [])
   
   return (
     <>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,28 +1,18 @@
-import {  SearchIcon } from 'lucide-react'
 import React, { useEffect, useState } from 'react'
+import {  SearchIcon } from 'lucide-react'
+import { useRecoilValue } from 'recoil'
+import { apiDataState } from '../apiState'
 
 function Search() {
     const [Searchquery, setSearchquery] = useState("")
-    const [Data, setData] = useState([])
+  
     const [FilterData, setFilterData] = useState([])
-    useEffect(() => {
-     const FetchedData = async () => { 
-      try {
-            let response = await fetch("http://localhost:3001/tests")
-        let data = await response.json()
-        console.log(data)
-        setData(data)
-        
-      } catch (error) {
-        console.error("Failed to fetch data" , error)
-      }
-    
-     }
-     FetchedData()
-    }, [])
+ 
+
+      const apiData = useRecoilValue(apiDataState)
 
     useEffect(() => {
-      const  filtereddata = Data.filter((item , i)=> {
+      const  filtereddata = apiData.filter((item , i)=> {
         if (item.name.toLowerCase().includes(Searchquery.toLowerCase())) {
           return true
         }
@@ -30,7 +20,7 @@ function Search() {
       console.log(filtereddata);
       
       setFilterData(filtereddata)
-    }, [Searchquery, Data])
+    }, [Searchquery,apiData])
     
 
   return (
@@ -45,15 +35,18 @@ function Search() {
           className="pl-10 pr-10 py-3 text-lg border-gray-200 focus:border-purple-500 focus:ring-purple-500"
           />
     </div>
-
-     <div>
-      {FilterData.map((item , i)=>(
+    
+    {Searchquery.length !== 0 ? (
+      <div>
+       <p>{FilterData.length} {FilterData.length === 1 ? 'result' : 'results'} found 
+  </p>
+      {FilterData.map((item , i)=>( 
         <div key={i}>
-           
-   <img src={item.icon} alt="" />
+          <p className='cursor-pointer'>{item.name}</p>
         </div>      
       ))}
      </div>
+    ) : null}
           </>
   
   )
